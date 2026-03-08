4a983abb5892432c1cf8cf91865a5986b05346f0
Manu <emmanuelkyalo91@gmail.com>
2026-03-08 09:54:07 +0300

    Environment Hardening: Implemented comprehensive global type fallbacks and prioritized local typeRoots in tsconfig to resolve IDE resolution failures.

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,18 +1,45 @@
-import * as React from 'react';
-
-declare global {
-    namespace JSX {
-        interface IntrinsicElements {
-            [elemName: string]: any;
-        }
+declare namespace JSX {
+    interface IntrinsicElements {
+        [elemName: string]: any;
     }
 }
 
 declare module 'react' {
-    export = React;
+    export type ReactNode = any;
+    export type ReactElement = any;
+    export function useState<T>(initialState: T | (() => T)): [T, (newState: T) => void];
+    export function useEffect(effect: () => void | (() => void), deps?: any[]): void;
+    export function useMemo<T>(factory: () => T, deps: any[]): T;
+    export function useCallback<T extends (...args: any[]) => any>(callback: T, deps: any[]): T;
+    export function useRef<T>(initialValue: T): { current: T };
+    // Add more common hooks as needed
+    const React: any;
+    export default React;
 }
 
 declare module 'react-dom' {
-    import * as ReactDOM from 'react-dom';
-    export = ReactDOM;
+    const ReactDOM: any;
+    export default ReactDOM;
+}
+
+declare module 'framer-motion' {
+    export const motion: any;
+    export const AnimatePresence: any;
+}
+
+declare module 'next/link' {
+    const Link: any;
+    export default Link;
+}
+
+declare module 'next/image' {
+    const Image: any;
+    export default Image;
+}
+
+declare module 'next/navigation' {
+    export function useRouter(): any;
+    export function usePathname(): any;
+    export function useSearchParams(): any;
+    export function notFound(): never;
 }
